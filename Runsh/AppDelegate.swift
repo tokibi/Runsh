eba841ca7edcb8de6f38499cac8eb6807902e078
tokibi <tatu014@gmail.com>
2018-04-28 12:30:59 +0900

    Initial Commit

---
+++ Runsh/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Runsh
+//
+//  Created by tokibi on 2018/04/28.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Runsh/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Runsh
+//
+//  Created by tokibi on 2018/04/28.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
